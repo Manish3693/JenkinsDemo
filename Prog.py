dded05b209eb5c118cac6527b29e4b8423212d15
Manish3693 <77501236+Manish3693@users.noreply.github.com>
2022-11-25 02:55:44 +0530

    Update Prog.py

--- Prog.py
+++ Prog.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-# Source code for summation of two numbers
 
-def summation(data):
-    return sum(data)
+def addition(n1, n2):
+    return (n1 + n2)
+
+def subtraction(n1, n2):
+    return (n1 - n2)
+
+def multiplication(n1, n2):
+    return (n1 * n2)
+
+def division(n1, n2):
+    if(n2!=0):
+        return (n1 / n2)
+    return 0
+    
