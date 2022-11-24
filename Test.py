a925c825b749786ad5ca06dc7729c2b390871de7
Manish3693 <77501236+Manish3693@users.noreply.github.com>
2022-11-25 02:55:14 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-# Test case for adding two numbers
+
 import unittest
 
-from Prog import summation
+from code import *
 
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
-        """
-        Test case to add two numbers
-        """
-        data = [20, 30]
-        result = summation(data)
-        self.assertEqual(result, 500)
+class Test(unittest.TestCase):
+    def test_addition(self):
+        data = [5, 10]
+        expected = 16
+        self.assertEqual(addition(data[0], data[1]), expected)
+
+    def test_subtraction(self):
+        data = [5, 10]
+        expected = -5
+        self.assertEqual(subtraction(data[0], data[1]), expected)
+
+    def test_multiplication(self):
+        data = [5, 10]
+        expected = 50
+        self.assertEqual(multiplication(data[0], data[1]), expected)
+
+    def test_division(self):
+        data = [10, 2]
+        expected = 5
+        self.assertAlmostEqual(division(data[0], data[1]), expected)
 
 if __name__ == '__main__':
     unittest.main()
